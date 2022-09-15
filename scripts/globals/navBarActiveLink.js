076a9b0356cc4a58f191c67dff87962c3bab98ef
JiXiangChua <jixiangchua2697@gmail.com>
2022-09-15 13:08:29 +0800

    Added nav bar to promo, cinema, check booking page

--- scripts/globals/navBarActiveLink.js
+++ scripts/globals/navBarActiveLink.js
@@ -1,7 +1,14 @@
-//console.log(window.location)
+//console.log(window.location);
 const activeLink = window.location.pathname;
 const navLinks = document.querySelectorAll("nav a");
 navLinks.forEach((link) => {
+  if (activeLink == "/IE4717/Project/") {
+    //to cater for the index path where index.html is not stated in the Url Query.
+    document
+      .querySelectorAll(".nav-items a")[0]
+      .classList.add("nav-item-active");
+    return;
+  }
   if (link.href.includes(`${activeLink}`)) {
     link.classList.add("nav-item-active");
   }
