967a5f3bc9176817020619127e77f82ee1b836bb
JiXiangChua <jixiangchua2697@gmail.com>
2022-09-25 14:33:00 +0800

    Implemented UI logic of seat selection page. Render the occupied seats upon loading the page. Save user selected seats to session storage only when user clicks "next" button. Able to restore the selected and occupied seats if user navigate back to this page.

--- scripts/pages/seatSelection.js
+++ scripts/pages/seatSelection.js
@@ -1,20 +1,75 @@
 const seatPlanContainer = document.querySelector(".seat-plan-container");
 const seats = document.querySelectorAll(".row .seat:not(.occupied)"); //select all the classes with seat that is not occupied
 const count = document.getElementById("count");
+const selectSeatsList = document.getElementById("selected-seats");
+const nextButton = document.getElementById("next-button");
+
+var updatedSelectedSeatList = [];
+
+var occupiedSeats = [
+  "A1",
+  "A2",
+  "A16",
+  "A17",
+  "B8",
+  "B9",
+  "B10",
+  "C14",
+  "C15",
+  "E19",
+  "E20",
+]; //take from database
+
+//restore already occupied seats:
+const renderOccupiedSeats = () => {
+  if (occupiedSeats !== null && occupiedSeats.length > 0) {
+    seats.forEach((seat) => {
+      if (occupiedSeats.includes(seat.getAttribute("data-value"))) {
+        seat.classList.add("occupied");
+      }
+    });
+  }
+};
 
 //update count
 const updateSelectedCount = () => {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
 
-  //   const seatsIndex = [...selectedSeats].map((seat) => {
-  //     return [...seats].indexOf(seat);
-  //   });
+  const selected = [];
 
-  //   sessionStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+  selectedSeats.forEach((seat) => {
+    selected.push(seat.getAttribute("data-value"));
+  });
 
+  //update count of selected seats
   const selectedSeatsCount = selectedSeats.length;
-
   count.innerText = selectedSeatsCount;
+
+  //display the seat number selected
+  if (selected.length > 0) {
+    const listOfSelectedSeats = selected.join(", ");
+
+    selectSeatsList.innerText = `Selected: ${listOfSelectedSeats}`;
+  } else {
+    selectSeatsList.innerText = "";
+  }
+
+  updatedSelectedSeatList = [...selected];
+};
+
+//restore user previously selected seats
+const renderPreviousSelectedSeats = () => {
+  const selectedSeats = JSON.parse(sessionStorage.getItem("selectedSeats"));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat) => {
+      if (selectedSeats.includes(seat.getAttribute("data-value"))) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  updateSelectedCount();
 };
 
 seatPlanContainer.addEventListener("click", (event) => {
@@ -24,8 +79,23 @@ seatPlanContainer.addEventListener("click", (event) => {
     !event.target.classList.contains("occupied")
   ) {
     event.target.classList.toggle("selected");
-    console.log(event.target.getAttribute("data-value")); //get the id of the seat selected
+    //console.log(event.target.getAttribute("data-value")); //get the id of the seat selected
   }
 
   updateSelectedCount();
 });
+
+nextButton.addEventListener("click", () => {
+  //save user seat selection:
+  sessionStorage.setItem(
+    "selectedSeats",
+    JSON.stringify(updatedSelectedSeatList)
+  );
+
+  //navigate to food page
+  window.location.href = "./food.html";
+});
+
+//functions that execute when page loads:
+renderOccupiedSeats();
+renderPreviousSelectedSeats();
