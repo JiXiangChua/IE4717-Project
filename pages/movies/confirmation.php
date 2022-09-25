c99ba6c81f13e884ab116b100c600a0999c25664
JiXiangChua <jixiangchua2697@gmail.com>
2022-09-25 23:41:31 +0800

    Added form validation to payment page. Added a confirmation php page but currently not in used.

---
+++ pages/movies/confirmation.php
@@ -0,0 +1,73 @@
+<?php
+
+$name = $_POST['cardHolder']; //must use square brackets!!
+$cardNumber = $_POST['cardNumber'];
+$expiryDate = $_POST['expiryDate'];
+$cvcNumber = $_POST['cvc'];
+$email = $_POST['email'];
+
+// @ $db = new mysqli('localhost', 'localhost', '', 'cineverseDB');
+// if (mysqli_connect_errno())
+// {
+//     echo "<script>console.log(Error)</script>";
+
+//     exit;
+// }
+?>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Cineverse - Booking Confirmation</title>
+    <link rel="stylesheet" href="../../styles/globals/main.css">
+    <link rel="stylesheet" href="../../styles/pages/movie-confirmation.css">
+    <script src="../../scripts/globals/navBarActiveLink.js" defer></script>
+</head>
+
+<body>
+    <div id="wrapper">
+        <nav class="nav-container">
+            <img id="cineverse-logo" src="../../images/icons/cineverse_logo.png" alt="cineverse_logo">
+            <div class="nav-items">
+                <a class="active" href="../../index.html">Home</a>
+                <a href="../movies/movies.html">Movies</a>
+                <a href="../cinemas/cinema.html">Cinemas</a>
+                <a href="../promotions/promotion.html">Promotions</a>
+                <a href="../checkbooking/checkbooking.html">Check Booking</a>
+            </div>
+        </nav>
+        <div class="container">
+            <div class="confirmation-container">
+                <h1>Booking Success</h1>
+                <div id="transaction-id">
+                    <p>Transaction No: 000111</p>
+                    <!-- TODO: change T No to variable -->
+                    <br />
+                    <p>E-tickets have been sent to your e-mail: abc@abc.com</p>
+                    <!-- TODO: change email to variable -->
+                </div>
+                <a href="../../index.html">
+                    <button type="button" class="accent-button done-button">Done</button>
+                </a>
+            </div>
+        </div>
+        <footer class="footer-container">
+            <img id="cineverse-logo" src="../../images/icons/cineverse_logo.png" alt="cineverse_logo">
+            <div class="link-container">
+                <a href="">About</a>
+                <a href="">FAQs</a>
+                <a href="">Contact Us</a>
+                <a href="">Terms of Service</a>
+            </div>
+            <div class="social-media-icons">
+                <img id="social-media-icon" src="../../images/icons/facebook-icon.png" alt="facebook-icon">
+                <img id="social-media-icon" src="../../images/icons/instagram-icon.png" alt="instagram-icon">
+            </div>
+            <p>&copy; COPYRIGHT 2022 IE4717 F38-DG05 | CHUA JI XIANG | TANG ZHAO SHENG </p>
+        </footer>
+    </div>
+    <script src="../../scripts/pages/confirmation.js"></script>
+</body>
+
+</html>
---
+++ scripts/pages/paymentValidation.js
@@ -0,0 +1,83 @@
+const cardHolderInput = document.getElementById("card-holder");
+const cardNumberInput = document.getElementById("card-number");
+const expiryDateInput = document.getElementById("expiry-date");
+const cvcNumberInput = document.getElementById("cvc");
+const emailInput = document.getElementById("email");
+
+expiryDateInput.min = new Date().toISOString().split("T")[0];
+
+const validateCardHolder = (event) => {
+  var name = event.target.value;
+  var validateExp = /^([A-Za-z\s]+)$/;
+
+  var index = name.search(validateExp);
+
+  if (index != 0) {
+    alert(
+      "You have entered your name wrongly.\n You can only input characters and spaces.\n Please check again!"
+    );
+    event.target.value = "";
+    event.target.focus();
+  }
+};
+
+const validateCardNumber = (event) => {
+  var cardNumber = event.target.value;
+  var validateExp = /^\d{16}$/;
+
+  var index = cardNumber.search(validateExp);
+
+  if (index != 0) {
+    alert(
+      "You have entered an invalid credit card number. \nPlease verify again."
+    );
+    event.target.value = "";
+    event.target.focus();
+    return false;
+  }
+};
+
+const validateExpiryDate = (event) => {
+  const currentDate = new Date();
+  const expiryDate3Months = new Date(
+    currentDate.setMonth(currentDate.getMonth() + 3)
+  );
+  var dateSelected = new Date(event.target.value);
+  if (expiryDate3Months > dateSelected) {
+    alert("Your card must not have an expiry date less than 3 months.");
+    event.target.value = ""; //reset the date
+    event.target.focus();
+  }
+};
+
+const validateCvcNumber = (event) => {
+  var cvc = event.target.value;
+  var validateExp = /^\d{3}$/;
+
+  var index = cvc.search(validateExp);
+
+  if (index != 0) {
+    alert("You have entered an invalid CVC number. \nPlease verify again.");
+    event.target.value = "";
+    event.target.focus();
+  }
+};
+
+const validateEmail = (event) => {
+  var email = event.target.value;
+  var validateExp = /^[\w-.]+@([A-za-z]+\.){1,3}[A-Za-z]{2,3}$/;
+
+  var index = email.search(validateExp);
+
+  if (index != 0) {
+    alert("You have entered an invalid email. Please verify again.");
+    event.target.value = "";
+    event.target.focus();
+  }
+};
+
+cardHolderInput.addEventListener("change", validateCardHolder);
+cardNumberInput.addEventListener("change", validateCardNumber);
+expiryDateInput.addEventListener("change", validateExpiryDate);
+cvcNumberInput.addEventListener("change", validateCvcNumber);
+emailInput.addEventListener("change", validateEmail);
